2622852ea4d127df8a54ac30a39118fb71f44bf2
Meekys <meekys@hotmail.com>
2016-10-12 21:49:39 +1030

    Changed package suffix

--- Scripts/Vsts-Pack.ps1
+++ Scripts/Vsts-Pack.ps1
@@ -2,4 +2,4 @@
     -projectName $env:SYSTEM_TEAMPROJECT `
     -configuration $env:BUILD_CONFIGURATION `
     -output $env:BUILD_ARTIFACTSTAGINGDIRECTORY `
-    -versionSuffix $env:BUILD_DEFINITIONVERSION
+    -versionSuffix $env:BUILD_BUILDNUMBER
