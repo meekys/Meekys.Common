e65b900f0331c3f4c63bf632ef351667b1836532
Meekys <meekys@hotmail.com>
2016-10-19 20:33:46 +1030

    Added debug output to Vsts-Pack

--- Scripts/Vsts-Pack.ps1
+++ Scripts/Vsts-Pack.ps1
@@ -1,6 +1,11 @@
 $revision = ($env:BUILD_DEFINITIONVERSION -split "\.")[-1]
 $tag = if ($env:VERSION_TAG -ne $null) { "$($env:VERSION_TAG)$revision"}
 
+Write-Host "BUILD_DEFINITION" $env:BUILD_DEFINITIONVERSION
+Write-Host "VERSION_TAG" $env:VERSION_TAG
+Write-Host "Revision: $revision"
+Write-Host "Tag: $tag"
+
 ./Scripts/Pack.ps1 `
     -projectName $env:SYSTEM_TEAMPROJECT `
     -configuration $env:BUILD_CONFIGURATION `
