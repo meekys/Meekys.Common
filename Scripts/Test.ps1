4b5465c880312681ee6cfb6a57a26b115e4d86ff
Meekys <meekys@hotmail.com>
2016-10-22 19:54:05 +1030

    Fixed Test script

--- Scripts/Test.ps1
+++ Scripts/Test.ps1
@@ -7,7 +7,7 @@ if (!(Test-Path test)) {
 }
 
 ls test\project.json -recurse | `
-    $ {
+    % {
         Write-Host -ForegroundColor Cyan "dotnet test $_"
         
         dotnet test $_ -c $configuration -xml "$($_.DirectoryName)\Test-Results.xml"
